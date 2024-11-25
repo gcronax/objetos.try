5eb68fc9a4b2b3f6ffe86dd271a868d5846afa37
gcronax <146924808+gcronax@users.noreply.github.com>
2024-11-25 20:11:34 +0100

    Update main.java
    asdasd

--- src/main.java
+++ src/main.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         Coche c1,c2,c3,c4;
         c1 =new Coche(100,"red");
-
+        System.out.println("se acelero a "+100+" color: "+"red");
+        System.out.print("dame cuanto tiempo va a estar conduciendo el ");
         Coche.muestraCoche();
-        System.out.println("se acelera 100 km/h y se conduce durante 20 min");
-
-        c1.setRecorre(0.333);
+        int reco=scan.nextInt();
+        c1.setRecorre(reco);
         System.out.println(c1.getAceleracion()+ "km/s velocidad y distancia km"+c1.getDistancia());
         System.out.println("se decelera 20 km/h y se conduce durante una hora");
         c1.setAceleracion(-20);
